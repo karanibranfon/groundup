19e7d35b35e8eb60b305a7591d649daac283136a
kaparo <karanibranfon@proton.me>
2026-03-08 22:37:44 +0300

    added the basic html pages

---
+++ telemed/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('send_feedback', views.feedback, name='feedback'),
+    path('help', views.help_view, name='help'),
+    path('pricing', views.pricing, name='pricing'),
+    path('login', views.login, name='login'),
+    path('about/testimonials', views.testimonials, name='testimonials'),
+    path('video', views.video, name='video'),
+    path('terms', views.terms, name='terms'),
+    path('privacy', views.privacy, name='privacy'),
+]
--- telemed/views.py
+++ telemed/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'telemed/index.html')
+
+
+def feedback(request):
+    return render(request, 'telemed/feedback.html')
+
+
+def help_view(request):
+    return render(request, 'telemed/help.html')
+
+
+def pricing(request):
+    return render(request, 'telemed/pricing.html')
+
+
+def login(request):
+    return render(request, 'telemed/login.html')
+
+
+def testimonials(request):
+    return render(request, 'telemed/testimonials.html')
+
+
+def video(request):
+    return render(request, 'telemed/video.html')
+
+
+def terms(request):
+    return render(request, 'telemed/terms.html')
+
+
+def privacy(request):
+    return render(request, 'telemed/privacy.html')
